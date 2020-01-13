c497f3e49be87874df54d29298a82fa01c584c20
bastazini <bastazini.vinicius@gmail.com>
2020-01-13 16:29:59 +0100

    Update Script_subset_traits.R

--- Script_subset_traits.R
+++ Script_subset_traits.R
@@ -1,7 +1,9 @@
+##Pacotes
 require(SYNCSA)
 
 ##Importando os dados
 comm=read.table(pipe("pbpaste"), sep="\t", header=T, row.names=1);comm
+comm=t(comm)
 traits=read.table(pipe("pbpaste"), sep="\t", header=T, row.names=1);traits
 
 ncomm <- nrow(comm)
@@ -17,5 +19,5 @@ traits.list <- sapply(as.list(seq_len(ncomm)), function(x) organize.syncsa(comm.
 
 																		   																		   check.comm = TRUE)$traits, simplify = FALSE)
 ##transformar uma das comunidades da lista em um matriz de trits
-traits1=as.matrix(traits.list[[10]])
+traits1=as.matrix(traits.list[[1]])
 
