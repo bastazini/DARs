c71b060d409184012f99623c1510e85e5dbce9af
bastazini <bastazini.vinicius@gmail.com>
2020-01-13 17:44:34 +0100

    teste

--- general script.R
+++ general script.R
@@ -1,9 +1,12 @@
+##Pacotes
 require(SYNCSA)
+require(vegan)
 
 ##Importando os dados
 comm=read.table(pipe("pbpaste"), sep="\t", header=T, row.names=1);comm
 comm=t(comm)
 traits=read.table(pipe("pbpaste"), sep="\t", header=T, row.names=1);traits
+traits=decostand(traits, "standardize")
 
 ncomm <- nrow(comm)
 ncomm
@@ -14,9 +17,11 @@ comm.list
 
 # Montar uma matriz de atributos para cada comunidade
 traits.list <- sapply(as.list(seq_len(ncomm)), function(x) organize.syncsa(comm.list[[x]], 
-																		   traits = traits, 
-
-																		   																		   check.comm = TRUE)$traits, simplify = FALSE)
+                                                                           traits = traits, 
+                                                                           
+                                                                           check.comm = TRUE)$traits, simplify = FALSE)
 ##transformar uma das comunidades da lista em um matriz de trits
-traits1=as.matrix(traits.list[[1]])
-
+traits1=as.matrix(traits.list[[4]]);traits1
+teste=quality_funct_space (traits1, nbdim=9, metric="Euclidean", plot="quality_funct_space")
+teste
+teste$meanSD
